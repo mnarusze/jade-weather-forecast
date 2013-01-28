2c64a1567baeef51934c9ab2476a004b44830751
Maciej Naruszewicz <maciek.naruszewicz@gmail.com>
2013-01-28 13:11:11 +0100

    Windows script refresh - regions still don't work..

--- src/JadeWeatherForecast/Scripts/StartContainers.bat
+++ src/JadeWeatherForecast/Scripts/StartContainers.bat
@@ -1,9 +1,6 @@
-set CLASSPATH="C:\Users\mnarusze\Documents\NetBeansProjects\JadeWeatherForecast\dist\JadeWeatherForecast.jar;C:\Program Files\Java\jade\lib\jade.jar"
-
 set AGENTS_AMOUNT=3
 set REGIONS_AMOUNT=2
 
 for /L %%i IN (1 1 %AGENTS_AMOUNT%) DO (
-	SET /A "REGION = %%i %% %REGIONS_AMOUNT%"
- 	start java -cp %CLASSPATH% jade.Boot -container -container-name station_%%i station_%%i_agent:JadeWeatherForecast.Agents.WeatherReporterAgent(%REGION%,0.0,10.0)
-)
+	SET /A "REGION=%REGIONS_AMOUNT% %% %%i"
+ 	start java jade.Boot -container -container-name station_%%i station_%%i_agent:JadeWeatherForecast.Agents.WeatherReporterAgent"(%REGION%,0.0,10.0)")
